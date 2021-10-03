d522fdc04e32ced093982c183c638f1557369cb2
jengi-afk <74922632+jengi-afk@users.noreply.github.com>
2021-10-03 18:04:33 -0400

    Agregar archivos de proyecto.

---
+++ IntroPilas/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroPilas
+{
+    public partial class Form1 : Form
+    {
+        Pila pila = new Pila();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pila.insertar(Int32.Parse(textBox1.Text));
+            textBox1.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pila.eliminar();
+        }
+    }
+}
---
+++ IntroPilas/Pila.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroPilas
+{
+    class Pila
+    {
+
+        static int maxpila = 5;
+        public int tope;
+        public int[] lista;
+
+
+        public Pila()
+        {
+            tope = -1;
+            lista = new int[maxpila];
+        }
+
+        bool vacia()
+        {
+            if (tope == -1)
+                return true;
+            else
+                return false;
+        }
+
+        bool llena()
+        {
+            if (tope == maxpila - 1)
+                return true;
+            else 
+                return false;
+
+        }
+
+
+        public void insertar(int elemento)
+        {
+            if (llena())
+            MessageBox.Show("La pila esta llena no se puede insertar mas elements");
+            else
+            {
+                tope++;
+                lista[tope] = elemento;
+
+            }
+        }
+         
+        public void eliminar()
+        {
+            if (vacia())
+                MessageBox.Show("la pila esta vacia, no se uede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("elemento eliminadod e la pila es: " + lista[tope]);
+                tope--;
+            }
+        }
+
+    }
+}
